49034da48479d03bf2c6df38c3f11ffb2e2376f6
Sho Matsuzaki <sho.g.matsuzaki@gmail.com>
2025-10-20 22:47:43 -0400

    Updated mobile design

--- js/main.js
+++ js/main.js
@@ -49,18 +49,18 @@ window.addEventListener('scroll', function() {
 	}
 
 	const photoSchedule1 = document.getElementById('photo-schedule1');
-	if (schedulePos < viewportHeight / 2) {
+	if (schedulePos < viewportHeight / 1.8) {
 		if (vw > 600) { // for desktop and tablet
 			photoSchedule1.style.transform = 'translate(0vw, 0vw) rotate(6deg)';
 		} else { // for mobile
-			photoSchedule1.style.transform = 'translate(-10px, 0px) rotate(6deg)';
+			photoSchedule1.style.transform = 'translate(-30px, -20px) rotate(6deg)';
 		}
 	}
 
 	const photoTravel1 = document.getElementById('photo-travel1');
 	const photoTravel2 = document.getElementById('photo-travel2');
 	const travelPos = document.getElementById('travel').getBoundingClientRect().top;
-	if (travelPos < viewportHeight / 2) {
+	if (travelPos < viewportHeight / 1.8) {
 		if (vw > 900) { // for desktop
 			photoTravel1.style.transform = 'translate(0vw, -4vw) rotate(6deg)';
 			photoTravel2.style.transform = 'translate(0vw, 0vw) rotate(-6deg)';
@@ -68,14 +68,14 @@ window.addEventListener('scroll', function() {
 			photoTravel1.style.transform = 'translate(-3vw, -4vw) rotate(6deg)';
 			photoTravel2.style.transform = 'translate(3vw, 0vw) rotate(-6deg)';
 		} else { // for mobile
-			photoTravel1.style.transform = 'translate(-150px, 70px) rotate(6deg)';
-			photoTravel2.style.transform = 'translate(150px, 0px) rotate(-6deg)';
+			photoTravel1.style.transform = 'translate(-120px, 70px) rotate(6deg)';
+			photoTravel2.style.transform = 'translate(120px, 0px) rotate(-6deg)';
 		}
 	}
 
 	const photoHotel1 = document.getElementById('photo-hotel1');
 	const hotelPos = document.getElementById('hotel').getBoundingClientRect().top;
-	if (hotelPos < viewportHeight / 2) {
+	if (hotelPos < viewportHeight / 1.8) {
 		if (vw > 600) { // for desktop and tablet
 			photoHotel1.style.transform = 'translate(0vw, 0vw) rotate(-6deg)';
 		} else { // for mobile
@@ -86,7 +86,7 @@ window.addEventListener('scroll', function() {
 	const photoTodo1 = document.getElementById('photo-todo1');
 	const photoTodo2 = document.getElementById('photo-todo2');
 	const todoPos = document.getElementById('to-do').getBoundingClientRect().top;
-	if (todoPos < viewportHeight / 2) {
+	if (todoPos < viewportHeight / 1.8) {
 		if (vw > 900) { // for desktop
 			photoTodo1.style.transform = 'translate(0vw, 0vw) rotate(-6deg)';
 			photoTodo2.style.transform = 'translate(0vw, -4vw) rotate(6deg)';
@@ -94,15 +94,15 @@ window.addEventListener('scroll', function() {
 			photoTodo1.style.transform = 'translate(-3vw, 0vw) rotate(-6deg)';
 			photoTodo2.style.transform = 'translate(3vw, -4vw) rotate(6deg)';
 		} else { // for mobile
-			photoTodo1.style.transform = 'translate(-150px, 0px) rotate(-6deg)';
-			photoTodo2.style.transform = 'translate(150px, 70px) rotate(6deg)';
+			photoTodo1.style.transform = 'translate(-120px, 0px) rotate(-6deg)';
+			photoTodo2.style.transform = 'translate(120px, 70px) rotate(6deg)';
 		}
 	}
 
 	const photoFAQ1 = document.getElementById('photo-faq1');
 	const photoFAQ2 = document.getElementById('photo-faq2');
 	const faqPos = document.getElementById('faq').getBoundingClientRect().top;
-	if (faqPos < viewportHeight / 2) {
+	if (faqPos < 0) {
 		if (vw > 600) { // for desktop and tablet
 			photoFAQ1.style.transform = 'translate(25vw, 0vw) rotate(-6deg)';
 			photoFAQ2.style.transform = 'translate(-25vw, -2vw) rotate(6deg)';
@@ -114,7 +114,7 @@ window.addEventListener('scroll', function() {
 
 	const photoRSVP1 = document.getElementById('photo-rsvp1');
 	const rsvpPos = document.getElementById('rsvp').getBoundingClientRect().top;
-	if (rsvpPos < viewportHeight / 2) {
+	if (rsvpPos < viewportHeight / 1.8) {
 		if (vw > 600) { // for desktop and tablet
 			photoRSVP1.style.transform = 'translate(0vw, 0vw) rotate(-6deg)';
 		} else { // for mobile
